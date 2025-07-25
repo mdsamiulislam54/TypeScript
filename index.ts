3235aa39c697dcba7b0ba34a0e2d9d9eaf78b435
mdsamiulislam54 <samiulm5332@gmail.com>
2025-07-25 12:37:55 +0600

    typescript environment setup and exploar data type with convert ts to js

---
+++ index.ts
@@ -0,0 +1,12 @@
+// let userName: string = 'MD Samiul Islam';
+// userName= ['MD', 'Samiul', 'Islam'].join(' ');
+// console.log(userName);
+
+// function typescript 
+
+// function addNumbers(num1: number,num2: number){
+//     console.log(num1 + num2);
+// }
+// addNumbers(20, 30); // 50
+// addNumbers(20, 40); // 2030
+
---
+++ type/type.ts
@@ -0,0 +1,24 @@
+// built-in type : number,string,boolean,void,any,undefined,null,never,object
+// user-defined type : enum, interface, class, array, object, tuple
+
+
+let userId: number;
+let userName: string
+let lastName: string;
+let isLoggedIn: boolean;
+let userDetails: object;
+let userArray: number[];
+
+userId = 101;
+userName = 'MD Samiul Islam';
+isLoggedIn = true;
+userDetails = {
+    id: userId,
+    name: userName,
+    loggedIn: isLoggedIn
+};
+userArray = [1,2,3,4,5,6,7,8,9,10];
+lastName = 'Islam';
+const fullName = userName.concat(' ',lastName)
+console.log(userId.toPrecision()); // 77
+console.log(fullName);
