c388edb6d505860386badd4b56f6c53bfdfc1f91
mdsamiulislam54 <samiulm5332@gmail.com>
2025-07-26 10:53:45 +0600

    inhreitance implement in class typescript

--- class.ts
+++ class.ts
@@ -1,22 +1,36 @@
-
 class Users {
-    // properties, constructors, and methods can be defined here
-    userName: string
-    userId:number;
-    age: number;
-
-    constructor (userName:string, userId:number,age:number){
-        this.userName=  userName;
-        this.userId = userId;   
-        this.age = age;
-    }
-
-    displayUserInfo =  ()=>{
-        console.log(`User Name: ${this.userName}, User ID: ${this.userId}, Age: ${this.age}`);
-    }
+  // properties, constructors, and methods can be defined here
+  userName: string
+  userId: number
+  age: number
+  constructor (userName: string, userId: number, age: number) {
+    this.userName = userName
+    this.userId = userId
+    this.age = age
+  }
+  displayUserInfo = () => {
+    console.log(
+      `User Name: ${this.userName}, User ID: ${this.userId}, Age: ${this.age}`
+    )
+  }
 }
 
-const user1 = new Users("MD Samiul Islam", 101, 25);
-const user2 = new Users("John Doe", 102, 30);
-user1.displayUserInfo();
-user2.displayUserInfo();
+const user1 = new Users('MD Samiul Islam', 101, 25)
+user1.displayUserInfo()
+
+// inheritance example
+class Admin extends Users {
+    adminId : number;
+    constructor(userName: string, userId: number, age:number, adminId: number) {
+       super(userName,userId, age);
+       this.adminId = adminId;
+    }
+    displayAdminInfo = () => {
+        console.log(
+            `Admin Name: ${this.userName}, Admin ID: ${this.adminId}, Age: ${this.age}`
+        );
+    }
+};
+
+const admin1 = new Admin ('Admin User', 201, 30, 10230181);
+admin1.displayAdminInfo()
