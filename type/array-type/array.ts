e6fdc05db9ede6121d0f1a809ea3c990af0465ec
mdsamiulislam54 <samiulm5332@gmail.com>
2025-07-26 09:09:17 +0600

    impletent tuple data type with typescript

--- type/array-type/array.ts
+++ type/array-type/array.ts
@@ -3,7 +3,7 @@ let numbers: number[]= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const allStudents = (student: string[])=>{
     return student.forEach((name:string)=>{
-        console.log(`Student Name: ${name}`);
+        console.log(`Student Name: ${name}`)
     })
 }
 
---
+++ type/tuple-data-type/tuple.ts
@@ -0,0 +1,2 @@
+const user: [number, string, boolean] = [101, 'MD Samiul Islam', true];
+console.log(`User ID: ${user[0]}, Name: ${user[1]}, Logged In: ${user[2]}`);
